9454bbda4787c36a74933514050c959d311ae01e
kochamlukasa <123631912+kochamlukasa@users.noreply.github.com>
2023-10-05 17:21:08 +0200

    Add files via upload

---
+++ pierdolcie sie/siar/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Odczytaj</title>
+    <link href="style.css" rel="stylesheet" type="text/css">
+
+</head>
+<body>
+<div class="gowno">
+        <h4></h4>
+</div>
+        <div id="container">
+        <fieldset id="f1">
+            <legend>Zdarzenia</legend>
+            <form method="POST" action="chuj.php">
+                <label> Data od:</label>
+            <input type="date" name="data1" ></input></br>
+            <label> Data do:</label>
+            <input type="date" name="data2" ></input></br>
+
+        <input type="submit" value="Pokaż"/>
+
+
+    </fieldset>
+    </div>
+
+</body>
+</html>
